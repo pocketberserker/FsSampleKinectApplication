fc9f1feeed72d05cdbf5940ed48fe5d3789c4406
pocketberserker <pocketberserker@gmail.com>
2011-12-02 18:08:00 +0900

    Kinect Runtimeの取得部分を追加

--- FsSampleKinectApplication/MainWindow.fs
+++ FsSampleKinectApplication/MainWindow.fs
@@ -2,11 +2,14 @@
 
 open System
 open System.Windows
+open Microsoft.Research.Kinect.Nui
 
 type MainWindow() =
 
   let window =
     Application.LoadComponent(new System.Uri("/FsSampleKinectApplication;component/MainWindow.xaml", System.UriKind.Relative)) :?> Window
+
+  let nui = Runtime.Kinects.[0]
 
   member this.Window = window
 
