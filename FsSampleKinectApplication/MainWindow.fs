5e19381dc6f1ce824d8932e1673382432eccc448
pocketberserker <pocketberserker@gmail.com>
2011-12-02 18:04:59 +0900

    XAMLからWindowを読み込み、アプリケーションを実行するコードを追加

---
+++ FsSampleKinectApplication/MainWindow.fs
@@ -0,0 +1,14 @@
+﻿module FsSampleKinectApplication
+
+open System
+open System.Windows
+
+type MainWindow() =
+
+  let window =
+    Application.LoadComponent(new System.Uri("/FsSampleKinectApplication;component/MainWindow.xaml", System.UriKind.Relative)) :?> Window
+
+  member this.Window = window
+
+[<STAThread>]
+(new MainWindow()).Window |> (new Application()).Run |> ignore
